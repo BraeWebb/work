21bf1318e81afd85caa166263fc6b9120cbfdcb2
BraeWebb <email@braewebb.com>
2017-01-08 21:26:22 +1000

    Flask Routing
    Updated the original flask routing file to reflect the changes and
    improvements to the data model and template files.

--- work.py
+++ work.py
@@ -1,41 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
+from invoice import Invoice, Item, Person
+from datetime import date
 
 app = Flask(__name__)
 
 
 @app.route('/invoices')
 def invoices():
-    return 'A list of invoices with preview data such as amount, date, etc. Includes a log item and log invoice button'
+    return render_template('invoices.html', invoices=reversed(Invoice.get_all()))
+
+@app.route('/api/log/item', methods=['POST'])
+def api_log_item():
+    item = Item.create(request.form.get('date'), request.form.get('description'), request.form.get('charge'))
+    return redirect(url_for('log_invoice'))
 
 
-@app.route('/invoice/<invoice>')
-def invoice(invoice):
-    return 'Display a HTML version of the invoice - includes a download, email, edit and pdf button'
+@app.route('/api/log/invoice', methods=['POST'])
+def api_log_invoice():
+    invoice = Invoice.create(request.form.get('date'), request.form.get('payer'),
+                             request.form.get('payee'), request.form.getlist('items'))
+    return redirect('/invoice/{}'.format(invoice.get_id()))
 
+def render(url, **funcs):
+    def inner_render(**args):
+        for key, arg in args.items():
+            if(funcs[key]):
+                args[key] = funcs[key](arg)
+        return render_template(url, **args)
+    return inner_render
 
-@app.route('/invoice/<invoice>.pdf')
-def invoice_pdf(invoice):
-    return open('{}.pdf'.format(invoice), 'rb').read(), 200, {'Content-Type': 'application/pdf',
-                                                              'Content-Disposition': 'inline; filename="{}.pdf"'
-                                                                  .format(invoice)}
-
-
-@app.route('/invoice/<invoice>/download')
-def invoice_download(invoice):
-    return open('{}.pdf'.format(invoice), 'rb').read(), 200, {'Content-Type': 'application/pdf',
-                                                              'Content-Disposition': 'attachment; filename="{}.pdf"'
-                                                                  .format(invoice)}
-
-
-@app.route('/invoice/<invoice>/edit')
-def edit_invoice(invoice):
-    return 'Edit the invoice information'
-
-
-@app.route('/log/invoice')
-def log_invoice():
-    return 'A form for logging a new invoice'
-
+app.add_url_rule('/invoice/<invoice>', 'view_invoice', lambda invoice: Invoice(invoice).html())
+app.add_url_rule('/invoice/<invoice>.pdf', 'view_invoice_pdf', lambda invoice: Invoice(invoice).pdf())
+app.add_url_rule('/invoice/<invoice>/download', 'download_invoice_pdf', lambda invoice: Invoice(invoice).download())
+app.add_url_rule('/invoice/<invoice>/email', 'email_invoice', render('invoices/email.html', invoice=Invoice))
+app.add_url_rule('/item/log', 'log_item', lambda: render_template('items/log.html', date=date.today()))
+app.add_url_rule('/invoice/log', 'log_invoice', lambda: render_template('invoices/log.html', date=date.today(), people=Person.get_all(), items=Item.get_unlogged()))
+app.add_url_rule('/api/email/invoice/<invoice>', 'send_email_invoice', lambda invoice: Invoice(invoice).email(request.form.get('body')))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
