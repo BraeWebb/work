701c096743e9b9771ee7abb4be55c71c6f903bad
BraeWebb <email@braewebb.com>
2017-01-12 16:43:40 +1000

    Refractor Database
    Refractored the database.py file with functional and structural changes.
    Removed unnessarcary lines to docstrings.
    Restructured many of the docstrings format to compact the comments.
    Reworded many docstrings.
    Added the new commit and rollback class methods to allow controlled committing
    and rollbacks using the database object.
    Changed the purpose of the commit method, previously the commit method
    handled the execution of SQL statements such as the insert statements that
    are immediantly committed. Now all queries are handled by the query method
    and commit only commits the queries.
    Added automatic commit and rollback functionality to the __exit__ magic
    method, if an error occurs within a with context block it is automatically
    rolled back, otherwise, it is automatically commited.
    Added a __repr__ magic method to allow for easier debugging.
    Removed the iterate method as it's method of iteration was dated and
    unpythonic and the iterate method has not been used in this project.
    Added the limit keyword to the query method as postgres does not support
    the LIMIT keyword this allows limiting.
    Allowed the query method to handle queries which do not return any data
    which previously raised a ProgrammingError.
    Allowed the exists method to handle empty with parameters to allow
    checking if a table exists at all.
    Ensured that booleans are actually returns from exists rather than falsey
    evaulation.
    Added the __bool__ magic method to check if the connection to the database
    has been closed.
    Added the __del__ magic method to allow for pythonic closing of the
    database as well as proper disposal of the object.

--- database.py
+++ database.py
@@ -1,76 +1,90 @@
 import psycopg2
+
 import config
 
+
 class database:
-
     def __init__(self):
-        '''
-        Establish a connection to the database and return the cursor
-
-        __init__() -> Cursor
-        '''
+        """Establish a connection to the database and initialize the class"""
         self.db = psycopg2.connect(host=config.postgres_host,
                                    user=config.postgres_user,
                                    password=config.postgres_password,
                                    database=config.postgres_database)
-        self.cur = self.db.cursor()
+        self.cursor = self.db.cursor()
 
-    def commit(self, query, *variables):
-        '''
-        Execute SQL statements to be commited as changes to the database
-
-        commit(str, str, str, ..., str) -> Cursor
-        '''
-        self.cur.execute(query, variables)
+    def commit(self):
+        """Commit the queries previously run on the database"""
         self.db.commit()
-        return self.cur
+
+    def rollback(self):
+        """Rollback the queries previously run on the database"""
+        self.db.rollback()
+
+    def close(self):
+        """Close the cursor object and the connection to the database
+
+        Used when closing the context manager or deleting the object
+        """
+        self.cursor.close()
+        self.db.close()
 
     def __enter__(self):
+        """Allows for the database to have context management"""
         return self
 
     def __exit__(self, exception_type, exception_val, trace):
+        """Allows for the database to have context management
+
+        If an error occurred rollback the database, otherwise, commits
+        """
+        if not exception_type:
+            self.commit()
+        else:
+            self.rollback()
         self.close()
 
-    def close(self):
-        '''
-        Close the connection to the database
+    def __repr__(self):
+        """Represent the database object as a string"""
+        data = [s.split(' ') for s in self.db.dsn.split('=')]
+        return 'Database(database={}, user={})'.format(data[1][0], data[2][0])
 
-        close() -> None
-        '''
-        self.cur.close()
-        self.db.close()
+    def query(self, query, *variables, limit=None):
+        """Execute a SQL query and return the results
 
-    def iterate(self, query, callback):
-        '''
-        Execute SQL query and call the callback function for each row with row
-        as a parameter
-
-        iterate(str, function) -> None
-        '''
-        self.cur.execute(query)
-        numrows = int(self.cur.rowcount)
-        for x in range(numrows):
-            row=self.cur.fetchone()
-            callback(row)
-        self.db.commit()
-        self.cur.close()
-
-    def query(self, query, *variables):
-        '''
-        Execute a SQL query and return all rows as an array
-
-        query(str, str, str, ..., str) -> tuple<tuple<obj>>
-        '''
-        self.cur.execute(query, variables)
-        return self.cur.fetchall()
+        query: a string SQL statement to execute on the database
+        *variables: the items to place in the query in the place of %s
+        limit: the maximum amount of rows to return
+        """
+        self.cursor.execute(query, variables)
+        try:
+            if limit:
+                return self.cursor.fetchmany(limit)
+            else:
+                return self.cursor.fetchall()
+        except psycopg2.ProgrammingError:
+            return None
 
     def exists(self, table, **where):
-        '''
-        Check if rows exist in a table satifying the given where clauses
+        """Check if rows exist in a table satisfying the given where clauses
 
-        exists(str, attr=str, attr=str, ..., attr=str) -> bool
-        '''
-        wheres = [str(attr)+'=\''+str(val)+'\'' for attr, val in where.items()]
-        self.cur.execute('SELECT COUNT(*) FROM {} WHERE {}'
-                         .format(table,' AND '.join(wheres)))
-        return self.cur.fetchone()[0]
+        table: the database table to check
+        **where: key value pairs of where conditions for locating the rows
+        """
+        # Format the list of where statements
+        wheres = ' AND '.join([str(attr) + '=\'' + str(val) + '\'' for attr, val in where.items()])
+
+        if where:
+            query = 'SELECT COUNT(*) FROM {} WHERE {}'.format(table, wheres)
+        else:
+            query = 'SELECT COUNT(*) FROM {}'.format(table, wheres)
+        self.cursor.execute(query)
+
+        return bool(self.cursor.fetchone()[0])
+
+    def __bool__(self):
+        """Return true if the database has an open connection, false otherwise"""
+        return not bool(self.db.closed)
+
+    def __del__(self):
+        """Closes all connections to the database when the object is deleted"""
+        self.close()
