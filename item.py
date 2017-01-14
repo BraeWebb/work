ca637e991b2239a068b30514ec19b43a3421f775
BraeWebb <email@braewebb.com>
2017-01-14 21:29:48 +1000

    Refractor Item
    Refractored the Item class with restructured and documentation.
    Added docstrings to each class and method in the Item class.
    Renamed the item_code class variable to code for simplicity.
    Separated SQL statements into temporary variables for a cleaner document.
    Made the constructor raise a Key Error if an invalid item code is
    provided.
    Created the _generate_code method as a helper method for create.
    Ensured that when an item is created it does not have an item code that is
    already in use.
    Following research, removed all standard getter methods in place of
    properties where needed. Also updated associated files to reflect the
    changes to the interface.

--- item.py
+++ item.py
@@ -3,55 +3,72 @@ from database import database
 import pylab
 
 class Item(object):
+    """An Item identified by the item code and stored within the database
+
+    An Item is a billable item with attributes of the date of logging this item, a item description and the charge for
+    the item.
+
+    This class provides an interface to the item model stored in the database
+    """
     def __init__(self, item_code):
-        self.item_code = item_code
+        """Initialize the class variables including the item code provided in the constructor and the date, description
+        and amount retrieved from the database
+        """
+        self.code = item_code
         with database() as db:
-            if db.exists('items', item_code = self.item_code):
-                self.date, self.description, self.charge = \
-                    db.query('SELECT date, description, charge FROM items WHERE item_code = %s', self.item_code)[0]
+            if db.exists('items', item_code = self.code):
+                sql = 'SELECT date, description, charge FROM items WHERE item_code = %s'
+                self.date, self.description, self.amount = db.query(sql, self.code, limit=1)[0]
+            else:
+                raise KeyError('No item with item code of {} exists within the database'.format(self.code))
+
+    @staticmethod
+    def _generate_code(self):
+        import random, string
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
 
     @classmethod
     def create(cls, date, description, charge):
-        import random, string
+        """Create a new item with the given information
+
+        Generate a random item code and associate it with the information provided in the database"""
         with database() as db:
-            id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
-            db.commit('INSERT INTO items (item_code, date, description, charge) VALUES (%s, %s, %s, %s)',
-                      id, date, description, charge)
-        return Item(id)
+            # Generate a new item code until one is generated which is not already in use
+            id = cls._generate_code()
+            while db.exists('items', item_code = id):
+                id = cls._generate_code()
+
+            # Insert the new item into the database
+            sql = 'INSERT INTO items (item_code, date, description, charge) VALUES (%s, %s, %s, %s)'
+            db.query(sql, id, date, description, charge)
+        return cls(id)
 
     @staticmethod
     def get_all():
+        """Retrieve an instance of all items that are currently stored in the database"""
         with database() as db:
             return [Item(item[0]) for item in db.query('SELECT item_code FROM items')]
 
     @staticmethod
     def get_unlogged():
+        """Retrieve an instance of all items which have not been added to an invoice currently in the database"""
         with database() as db:
-            return [Item(item[0]) for item in db.query('SELECT item_code FROM items WHERE item_code NOT IN (SELECT item_code FROM invoice_items)')]
-
-    def get_id(self):
-        return self.item_code
-
-    def get_date(self):
-        return self.date
-
-    def get_amount(self):
-        return self.charge
-
-    def get_description(self):
-        return self.description
+            sql = 'SELECT item_code FROM items WHERE item_code NOT IN (SELECT item_code FROM invoice_items)'
+            return [Item(item[0]) for item in db.query(sql)]
 
     def __lt__(self, other):
-        return self.get_date() < other.get_date()
+        """The __lt__ magic method allows items to be sorted according to their date"""
+        return self.date < other.date
 
     @staticmethod
     def statistics():
+        """Generate a pylab figure demonstrating the amount charged for each item over time"""
         dates = []
         costs = []
         pylab.figure()
         for item in sorted(Item.get_all()):
-            dates.append(item.get_date())
-            costs.append(item.get_amount())
+            dates.append(item.date)
+            costs.append(item.amount)
         pylab.legend(('Item Charges', ))
         pylab.plot(dates, costs)
         return pylab
