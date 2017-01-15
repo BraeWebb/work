59f55a0b0222fbb08fd65f0c5f543fd6d0a349f3
BraeWebb <email@braewebb.com>
2017-01-15 14:14:46 +1000

    Refractor Person
    Added docstrings to all classes and methods relating to the person class.
    Ensured that a person exists before attempting to retrieve the person data
    from the database, if a person does not exist in the database then it
    raises a key error.
    Added the create method to the Person class to allow easy insertion of new
    people into the database.
    Added the delete method to the Person class to allow easy deletion of
    people from the database.
    Following research, removed the getter methods and changed any variables
    requiring getter and setter methods to properties.
    Removed the __str__ method and started using the name variable to replace
    this functionality.
    Updated the template files to match the changes to this model as well as
    previous model updates that were missed.

--- person.py
+++ person.py
@@ -1,24 +1,37 @@
 from database import database
 
 class Person(object):
+    """An interface to the person database model
+
+    Contains information regarding the persons name, address and email address
+    """
     def __init__(self, name):
+        """Initialize the class by retrieving the person data from the database based on the name"""
         self.name = name
         with database() as db:
-            self.address, self.email = db.query('SELECT address, email FROM persons WHERE person_name = %s', self.name)[0]
+            if(db.exists('persons', person_name = self.name)):
+                self.address, self.email = db.query('SELECT address, email FROM persons WHERE person_name = %s', self.name)[0]
+            else:
+                raise KeyError('No person with the name {} exists within the database'.format(self.name))
+
+    @classmethod
+    def create(cls, name, address, email):
+        """Add a new person to the database if the name is not already in use"""
+        with database() as db:
+            if not db.exists('persons', person_name = name):
+                sql = 'INSERT INTO persons (person_name, address, email) VALUES (%s, %s, %s)'
+                db.query(sql, name, address, email)
+            else:
+                raise KeyError('A person with the name {} already exists within the database'.format(name))
+        return cls(name)
+
+    def delete(self):
+        """Remove the person data from the database"""
+        with database() as db:
+            db.query('DELETE FROM persons WHERE person_name = %s', self.name)
 
     @staticmethod
     def get_all():
+        """Retrieve all persons stored in the database"""
         with database() as db:
             return [Person(person[0]) for person in db.query('SELECT person_name FROM persons')]
-
-    def get_name(self):
-        return self.name
-
-    def get_address(self):
-        return self.address
-
-    def get_email(self):
-        return self.email
-
-    def __str__(self):
-        return self.name
