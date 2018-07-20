41841b05f73211eac934f203107f6b2ed4bdf386
BraeWebb <email@braewebb.com>
2018-07-21 09:47:55 +1000

    Template Configuration File
    Added a template configuration file for easy setup.

---
+++ template_config.py
@@ -0,0 +1,17 @@
+postgres_host = '127.0.0.1'
+postgres_user = 'postgres'
+postgres_password = ''
+postgres_database = 'work'
+
+secret_key = ''
+
+smtp_server = 'smtp.gmail.com'
+smtp_port = 465
+email_login = ''
+email_password = ''
+
+# ABSTRACT BELOW
+abn = 0
+payment_details = \
+'''
+'''
